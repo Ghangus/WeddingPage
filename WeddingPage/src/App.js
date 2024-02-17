8a5b5aba8ded31df8fa39a82c04c4c225309e488
FredrikCarlsson <fredrik.carlsson.92@gmail.com>
2024-02-17 21:26:03 +0100

    Added QR code and settings filed

--- WeddingPage/src/App.js
+++ WeddingPage/src/App.js
@@ -3,22 +3,39 @@ import "./index.css";
 
 function WeddingPicture({ picturePath }) {
   return (
-    <div className="flex justify-center h-screen">
-      <img className="justify-center rounded" src={picturePath} alt="Wedding" />
+    <div class="overflow-hidden w-screen h-screen flex justify-center items-center">
+      <img
+        class="w-full h-full object-scale-down"
+        src={picturePath}
+        alt="Wedding"
+      />
     </div>
   );
 }
 
-function QrCodeHeader({ headerText }) {
+function QrCodeHeader() {
   return (
-    <div class="App-header">
-      <h1 className="text-center text-black text-3xl">{headerText}</h1>
+    <div class="flex gap-4 absolute  p-4 rounded-md right-4 bottom-4">
+      <div class="p-4 bg-white rounded-md max-w-52 backdrop-blur">
+        <h1 className="text-black text-xl">
+          Skanna och ladda upp era bilder! 📷
+        </h1>
+      </div>
+      <div class="p-1 bg-white rounded-md backdrop-blur">
+        <img
+          class="rounded object-contain w-20"
+          src={"qr_code_newtechcarlsson.png"}
+          alt="Wedding"
+        />
+      </div>
     </div>
   );
 }
 
 function fetchImagePath(setImageCallback) {
   const apiUrl = "http://localhost:8080/get_random_file";
+  const image1 = "/defaultImages/IMG_3946.png";
+  const image2 = "/defaultImages/IMG_3951.jpg";
 
   fetch(apiUrl)
     .then((response) => {
@@ -32,7 +49,10 @@ function fetchImagePath(setImageCallback) {
       setImageCallback(`/WeddingImages/${data}`);
     })
     .catch((error) => {
-      console.error("Error fetching image path:", error);
+      console.error("Error fetching image path using default images:", error);
+      return Math.random() > 0.5
+        ? setImageCallback(image1)
+        : setImageCallback(image2);
     });
 }
 
@@ -50,8 +70,11 @@ function TestButton({ onButtonClick }) {
 }
 
 function App() {
-  const [headerText, setHeaderText] = useState("Ladda upp era bilder här!");
-  const [weddingPicturePath, setPicturePath] = useState("/WeddingImages/IMG_3884.jpeg");
+  const [weddingPicturePath, setPicturePath] = useState(
+    "/WeddingImages/IMG_3884.jpeg",
+  );
+
+  const [showDev, setShowDev] = useState(false);
 
   // Wrap fetchImagePath call to provide it with setPicturePath
   const handleButtonClick = () => fetchImagePath(setPicturePath);
@@ -67,20 +90,27 @@ function App() {
 
     // Clear the interval when the component unmounts
     return () => clearInterval(interval);
-   }, []);
+  }, []);
 
-   useEffect(() => {
-  	console.log(`Updated picture path: ${weddingPicturePath}`);
-   }, [weddingPicturePath]);
+  useEffect(() => {
+    console.log(`Updated picture path: ${weddingPicturePath}`);
+  }, [weddingPicturePath]);
 
   return (
-    <div class="bg-gray-600">
-      {/* <QrCodeHeader headerText={headerText} /> */}
-      {/* <TestButton onButtonClick={handleButtonClick} /> */}
+    <div class="bg-black flex items-center">
+      <div class="absolute flex gap-2 items-center top-3 right-3 transition">
+        {showDev && <div class="bg-white/50 rounded-md px-4 py-1">hej</div>}
+        <div
+          class="rounded-full p-2 text-white hover:bg-gray-300/30 cursor-pointer"
+          onClick={() => setShowDev(!showDev)}
+        >
+          ⚙️
+        </div>
+      </div>
       <WeddingPicture picturePath={weddingPicturePath} />
+      <QrCodeHeader headerText />
     </div>
   );
 }
 
 export default App;
-
